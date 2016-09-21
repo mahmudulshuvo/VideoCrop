1b9d8c210b852a6f650888ab0012827ace7cf6c4
mahmudulshuvo <shuvo0201@gmail.com>
2016-09-21 13:28:26 +0600

    Initial Commit

---
+++ VideoCrop/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VideoCrop
+//
+//  Created by SHUVO on 9/21/16.
+//  Copyright © 2016 SHUVO. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
